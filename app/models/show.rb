62bc456fe2c6454da7303485c41943be4d71033f
Joshua Tan <jtsarvida@gmail.com>
2018-11-21 11:11:04 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -12,4 +12,10 @@ class Show < ActiveRecord::Base
   def self.lowest_rating
     self.minimum(:rating)
   end 
+  
+  def self.least_popular_show
+    not_popular_show = Show.find_by(self.lowest_rating)
+    not_popular_show
+  end 
+  
 end
